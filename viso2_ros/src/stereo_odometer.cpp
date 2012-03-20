3cf865db128d4cb3f83c977f8d02d7bfbc993cf1
Stephan Wirth <stephan.wirth@uib.es>
2012-03-20 10:40:58 +0100

    stereo_processor factored out of odometer

--- viso2_ros/src/stereo_odometer.cpp
+++ viso2_ros/src/stereo_odometer.cpp
@@ -1,49 +1,29 @@
 
 #include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <sensor_msgs/image_encodings.h>
 #include <nav_msgs/Odometry.h>
 
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/exact_time.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <image_transport/subscriber_filter.h>
+#include <sensor_msgs/image_encodings.h>
+
 #include <image_geometry/stereo_camera_model.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <cv_bridge/cv_bridge.h>
 
+#include "stereo_processor.h"
+
 #include "viso_stereo.h"
 
-// for sync checking
-void increment(int* value)
+namespace viso2_ros
 {
-  ++(*value);
-}
 
-class StereoOdometer
+class StereoOdometer : public StereoProcessor
 {
 
 private:
 
-  // publisher and subscriber
+  // publisher
   ros::Publisher odom_pub_;
   ros::Publisher pose_pub_;
-  image_transport::SubscriberFilter left_sub_, right_sub_;
-  message_filters::Subscriber<sensor_msgs::CameraInfo> left_info_sub_, right_info_sub_;
-  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ExactPolicy;
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ApproximatePolicy;
-  typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
-  typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
-  boost::shared_ptr<ExactSync> exact_sync_;
-  boost::shared_ptr<ApproximateSync> approximate_sync_;
-  int queue_size_;
-
-  // for sync checking
-  ros::WallTimer check_synced_timer_;
-  int left_received_, right_received_, left_info_received_, right_info_received_, all_received_;
 
   // tf related
   std::string frame_id_;
@@ -62,8 +42,7 @@ private:
 
 public:
 
-  StereoOdometer(const std::string& transport) :
-    left_received_(0), right_received_(0), left_info_received_(0), right_info_received_(0), all_received_(0)
+  StereoOdometer(const std::string& transport) : StereoProcessor(transport)
   {
     // Read local parameters
     ros::NodeHandle local_nh("~");
@@ -73,51 +52,6 @@ public:
     local_nh.param("child_frame_id", child_frame_id_, std::string("/base_link"));
     local_nh.param("publish_tf", publish_tf_, true);
     
-    // Resolve topic names
-    ros::NodeHandle nh;
-    std::string stereo_ns = nh.resolveName("stereo");
-    std::string left_topic = ros::names::clean(stereo_ns + "/left/" + nh.resolveName("image"));
-    std::string right_topic = ros::names::clean(stereo_ns + "/right/" + nh.resolveName("image"));
-
-    std::string left_info_topic = stereo_ns + "/left/camera_info";
-    std::string right_info_topic = stereo_ns + "/right/camera_info";
-
-    ROS_INFO("Subscribing to:\n\t* %s\n\t* %s\n\t* %s\n\t* %s", 
-        left_topic.c_str(), right_topic.c_str(),
-        left_info_topic.c_str(), right_info_topic.c_str());
-
-    // Subscribe to four input topics.
-    image_transport::ImageTransport it(nh);
-    left_sub_.subscribe(it, left_topic, 1, transport);
-    right_sub_.subscribe(it, right_topic, 1, transport);
-    left_info_sub_.subscribe(nh, left_info_topic, 1);
-    right_info_sub_.subscribe(nh, right_info_topic, 1);
-
-    // Complain every 15s if the topics appear unsynchronized
-    left_sub_.registerCallback(boost::bind(increment, &left_received_));
-    right_sub_.registerCallback(boost::bind(increment, &right_received_));
-    left_info_sub_.registerCallback(boost::bind(increment, &left_info_received_));
-    right_info_sub_.registerCallback(boost::bind(increment, &right_info_received_));
-    check_synced_timer_ = nh.createWallTimer(ros::WallDuration(15.0),
-                                             boost::bind(&StereoOdometer::checkInputsSynchronized, this));
-
-    // Synchronize input topics. Optionally do approximate synchronization.
-    local_nh.param("queue_size", queue_size_, 5);
-    bool approx;
-    local_nh.param("approximate_sync", approx, false);
-    if (approx)
-    {
-      approximate_sync_.reset( new ApproximateSync(ApproximatePolicy(queue_size_),
-                                                   left_sub_, right_sub_, left_info_sub_, right_info_sub_) );
-      approximate_sync_->registerCallback(boost::bind(&StereoOdometer::imageCb, this, _1, _2, _3, _4));
-    }
-    else
-    {
-      exact_sync_.reset( new ExactSync(ExactPolicy(queue_size_),
-                                       left_sub_, right_sub_, left_info_sub_, right_info_sub_) );
-      exact_sync_->registerCallback(boost::bind(&StereoOdometer::imageCb, this, _1, _2, _3, _4));
-    }
-
     // advertise
     odom_pub_ = local_nh.advertise<nav_msgs::Odometry>("odometry", 1);
     pose_pub_ = local_nh.advertise<geometry_msgs::PoseStamped>("pose", 1);
@@ -162,14 +96,13 @@ protected:
     local_nh.getParam("bucket_height", bucketing.bucket_height);
   }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& l_image_msg,
-               const sensor_msgs::ImageConstPtr& r_image_msg,
-               const sensor_msgs::CameraInfoConstPtr& l_info_msg,
-               const sensor_msgs::CameraInfoConstPtr& r_info_msg)
+  void imageCallback(
+      const sensor_msgs::ImageConstPtr& l_image_msg,
+      const sensor_msgs::ImageConstPtr& r_image_msg,
+      const sensor_msgs::CameraInfoConstPtr& l_info_msg,
+      const sensor_msgs::CameraInfoConstPtr& r_info_msg)
   {
  
-    ++all_received_; // For error checking
-
     // create odometer if not exists
     if (!visual_odometer_)
     {
@@ -294,32 +227,9 @@ protected:
     }
   }
 
-  void checkInputsSynchronized()
-  {
-    int threshold = 3 * all_received_;
-    if (left_received_ >= threshold || right_received_ >= threshold || 
-        left_info_received_ >= threshold || right_info_received_ >= threshold) {
-      ROS_WARN("[stereo_odometer] Low number of synchronized left/right/left_info/right_info tuples received.\n"
-               "Left images received:       %d (topic '%s')\n"
-               "Right images received:      %d (topic '%s')\n"
-               "Left camera info received:  %d (topic '%s')\n"
-               "Right camera info received: %d (topic '%s')\n"
-               "Synchronized tuples: %d\n"
-               "Possible issues:\n"
-               "\t* stereo_image_proc is not running.\n"
-               "\t  Does `rosnode info %s` show any connections?\n"
-               "\t* The cameras are not synchronized.\n"
-               "\t  Try restarting stereo_odometer with parameter _approximate_sync:=True\n"
-               "\t* The network is too slow. One or more images are dropped from each tuple.\n"
-               "\t  Try restarting stereo_odometer, increasing parameter 'queue_size' (currently %d)",
-               left_received_, left_sub_.getTopic().c_str(),
-               right_received_, right_sub_.getTopic().c_str(),
-               left_info_received_, left_info_sub_.getTopic().c_str(),
-               right_info_received_, right_info_sub_.getTopic().c_str(),
-               all_received_, ros::this_node::getName().c_str(), queue_size_);
-    }
-  }
 };
+
+} // end of namespace
 
 
 int main(int argc, char **argv)
@@ -336,7 +246,7 @@ int main(int argc, char **argv)
   }
 
   std::string transport = argc > 1 ? argv[1] : "raw";
-  StereoOdometer odometer(transport);
+  viso2_ros::StereoOdometer odometer(transport);
   
   ros::spin();
   return 0;
