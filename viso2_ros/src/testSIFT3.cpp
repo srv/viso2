9d18a220072e04fff4e7a3f5a1984e867ed7a93b
Bo Miquel Nordfeldt <bomiquel@gmail.com>
2021-04-16 09:17:24 +0200

    New quad matching working

--- viso2_ros/src/testSIFT3.cpp
+++ viso2_ros/src/testSIFT3.cpp
@@ -46,19 +46,23 @@ bool firstTime = true ;
 
 int k = 2 ;
 
+double Time_ImageCurrent ;
+double Time_ImagePrevious ;
+
 /////////////////////////////////////////////////////////////////
 /////////////////////////Callback////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
 void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_msgs::ImageConstPtr& r_image_msg){
 
+    Time_ImageCurrent = ros::Time::now().toSec() ;
 
     /////////////////////////////////////////////////////////////////
     /////////////////Local variable declaration//////////////////////
     /////////////////////////////////////////////////////////////////
 
     // Vectors
-    vector<vector<cv::DMatch>> left_matches, right_matches, previous_matches, current_vmatches ;
+    vector<vector<cv::DMatch>> left_matches, right_matches, previous_matches, current_matches ;
 
     vector<KeyPoint> l_curr_kpts, r_curr_kpts ;
 
@@ -70,6 +74,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
 
     vector<KeyPoint> r_pre_kpts_aft_F_aft_right_match, r_curr_kpts_aft_match ;
     vector<KeyPoint> r_pre_kpts_aft_H, r_curr_kpts_aft_H ;
+
+    vector<KeyPoint> r_curr_kpts_aft_H_aft_current_match, l_curr_kpts_aft_H_aft_current_match ;
+    vector<KeyPoint> r_curr_kpts_aft_F, l_curr_kpts_aft_F ;
 
     // Matrix
     Mat left_img, right_img ;
@@ -91,6 +98,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
     Mat r_pre_desc_aft_H, r_curr_desc_aft_H ;
     Mat right_RANSACinliersMask ;
 
+    Mat r_curr_desc_aft_H_aft_current_match, l_curr_desc_aft_H_aft_current_match ;
+    Mat current_F ;
+    Mat r_curr_desc_aft_F, l_curr_desc_aft_F ;
+    Mat current_RANSACinliersMask ;
+
     // Pointers
     cv_bridge::CvImageConstPtr l_cv_ptr, r_cv_ptr ;
     Ptr<SIFT> sift ;
@@ -105,6 +117,13 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
 
     vector<cv::Point2f> r_pre_coord_aft_F_aft_right_match, r_curr_coord_aft_match ;
     vector<cv::Point2f> r_pre_coord_aft_H, r_curr_coord_aft_H ;
+
+    vector<cv::Point2f> r_curr_coord_aft_H_aft_current_match, l_curr_coord_aft_H_aft_current_match ;
+    vector<cv::Point2f> r_curr_coord_aft_F, l_curr_coord_aft_F ;
+
+    // Iterators
+    int i ;
+    int j;
 
     /////////////////////////////////////////////////////////////////
     ////////////Transform image from ROS tu OpenCV///////////////////
@@ -149,7 +168,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         left_desc_matcher->knnMatch(l_curr_desc, l_pre_desc, left_matches, k, cv::noArray(), true) ;
 
         // Obtaining keypoints, coordinates and descriptors of the left side after matching 
-        for(int i = 0; i < left_matches.size(); i++){
+        for(i = 0; i < left_matches.size(); i++){
 
             l_curr_kpts_aft_match.push_back(l_curr_kpts[left_matches[i][0].queryIdx]) ;
             l_curr_desc_aft_match.push_back(l_curr_desc.row(left_matches[i][0].queryIdx)) ;
@@ -161,12 +180,12 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
 
         }
 
-        ROS_INFO("Current left keypoints: %d", l_curr_kpts.size()) ;
-        ROS_INFO("Previous left keypoints: %d", l_pre_kpts.size()) ;
-        ROS_INFO("Current left keypoints after matches: %d", l_curr_kpts_aft_match.size()) ;
-        ROS_INFO("Previous left matches keypoints: %d", l_pre_kpts_aft_match.size()) ;
-        ROS_INFO("Current left matches descriptors: %d", l_curr_desc_aft_match.size()) ;
-        ROS_INFO("Previous left matches descriptors: %d", l_pre_desc_aft_match.size()) ;
+        // ROS_INFO("Current left keypoints: %d", l_curr_kpts.size()) ;
+        // ROS_INFO("Previous left keypoints: %d", l_pre_kpts.size()) ;
+        // ROS_INFO("Current left keypoints after matches: %d", l_curr_kpts_aft_match.size()) ;
+        // ROS_INFO("Previous left matches keypoints: %d", l_pre_kpts_aft_match.size()) ;
+        // ROS_INFO("Current left matches descriptors: %d", l_curr_desc_aft_match.size()) ;
+        // ROS_INFO("Previous left matches descriptors: %d", l_pre_desc_aft_match.size()) ;
 
         // Compute left Homography
         try{
@@ -180,7 +199,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         }
 
         // Obtaining keypoints, coordinates and descriptors after homography
-        for(int i = 0; i < left_RANSACinliersMask.rows; i++){
+        for(i = 0; i < left_RANSACinliersMask.rows; i++){
             if(left_RANSACinliersMask.at<bool>(i, 0) == 1){
 
                 l_curr_kpts_aft_H.push_back(l_curr_kpts_aft_match[i]) ;
@@ -194,9 +213,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
             }
         }
 
-        ROS_INFO("Current left keypoints after H: %d", l_curr_kpts_aft_H.size()) ;
-        ROS_INFO("Previous left keypoints after H: %d", l_pre_kpts_aft_H.size()) ;
-        ROS_INFO("-----------------------------------------------") ;
+        // ROS_INFO("Current left keypoints after H: %d", l_curr_kpts_aft_H.size()) ;
+        // ROS_INFO("Previous left keypoints after H: %d", l_pre_kpts_aft_H.size()) ;
+        // ROS_INFO("-----------------------------------------------") ;
 
 
         /////////////////////////////////////////////////////////////////
@@ -208,7 +227,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         previous_desc_matcher->knnMatch(l_pre_desc_aft_H, r_pre_desc, previous_matches, k, cv::noArray(), true) ;
 
         // Obtaining keypoints, coordinates and descriptors of the previous side after matching 
-        for(int i = 0; i < previous_matches.size(); i++){
+        for(i = 0; i < previous_matches.size(); i++){
 
             l_pre_kpts_aft_H_aft_previous_match.push_back(l_pre_kpts_aft_H[previous_matches[i][0].queryIdx]) ;
             l_pre_desc_aft_H_aft_previous_match.push_back(l_pre_desc_aft_H.row(previous_matches[i][0].queryIdx)) ;
@@ -220,10 +239,10 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
  
         }
 
-        ROS_INFO("Previous left keypoints after H: %d", l_pre_kpts_aft_H.size()) ;
-        ROS_INFO("Previous right keypoints: %d", r_pre_kpts.size()) ;
-        ROS_INFO("Previous left keypoints after H and after match : %d", l_pre_kpts_aft_H_aft_previous_match.size() ) ;
-        ROS_INFO("Previous right keypoints after match: %d", r_pre_kpts_aft_match.size()) ;
+        // ROS_INFO("Previous left keypoints after H: %d", l_pre_kpts_aft_H.size()) ;
+        // ROS_INFO("Previous right keypoints: %d", r_pre_kpts.size()) ;
+        // ROS_INFO("Previous left keypoints after H and after match : %d", l_pre_kpts_aft_H_aft_previous_match.size() ) ;
+        // ROS_INFO("Previous right keypoints after match: %d", r_pre_kpts_aft_match.size()) ;
 
         // Compute previous Fundamental Matrix
         try{
@@ -237,7 +256,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         }
 
         // Obtaining keypoints, coordinates and descriptors of the previous side after fundamental matrix 
-        for(int i = 0; i < previous_RANSACinliersMask.rows; i++){
+        for(i = 0; i < previous_RANSACinliersMask.rows; i++){
             if(previous_RANSACinliersMask.at<bool>(i, 0) == 1){
 
 
@@ -252,9 +271,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
             }
         }
 
-        ROS_INFO("Previous left keypoints after F: %d", l_pre_kpts_aft_F.size()) ;
-        ROS_INFO("Previous right keypoints after F: %d", r_pre_kpts_aft_F.size()) ;
-        ROS_INFO("-----------------------------------------------") ;
+        // ROS_INFO("Previous left keypoints after F: %d", l_pre_kpts_aft_F.size()) ;
+        // ROS_INFO("Previous right keypoints after F: %d", r_pre_kpts_aft_F.size()) ;
+        // ROS_INFO("-----------------------------------------------") ;
 
         /////////////////////////////////////////////////////////////////
         /////////////////////////Right side//////////////////////////////
@@ -265,7 +284,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         right_desc_matcher->knnMatch(r_pre_desc_aft_F, r_curr_desc, right_matches, k, cv::noArray(), true) ;
 
         // Obtaining keypoints, coordinates and descriptors of the right side after matching 
-        for(int i = 0; i < right_matches.size(); i++){
+        for(i = 0; i < right_matches.size(); i++){
 
             r_pre_kpts_aft_F_aft_right_match.push_back(r_pre_kpts_aft_F[right_matches[i][0].queryIdx]) ;
             r_pre_desc_aft_F_aft_right_match.push_back(r_pre_desc_aft_F.row(right_matches[i][0].queryIdx)) ;
@@ -277,10 +296,10 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
  
         }
         
-        ROS_INFO("Previous right keypoints after F: %d", r_pre_kpts_aft_F.size()) ;
-        ROS_INFO("Current right keypoints: %d", r_curr_kpts.size()) ;
-        ROS_INFO("Previous left keypoints after H and after match : %d", r_pre_kpts_aft_F_aft_right_match.size() ) ;
-        ROS_INFO("Previous right keypoints after match: %d", r_curr_kpts_aft_match.size()) ;
+        // ROS_INFO("Previous right keypoints after F: %d", r_pre_kpts_aft_F.size()) ;
+        // ROS_INFO("Current right keypoints: %d", r_curr_kpts.size()) ;
+        // ROS_INFO("Previous right keypoints after H and after match : %d", r_pre_kpts_aft_F_aft_right_match.size() ) ;
+        // ROS_INFO("current right keypoints after match: %d", r_curr_kpts_aft_match.size()) ;
 
         // Compute right Homography
         try{
@@ -294,7 +313,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
         }
 
         // Obtaining keypoints, coordinates and descriptors of the right side after homography
-        for(int i = 0; i < right_RANSACinliersMask.rows; i++){
+        for(i = 0; i < right_RANSACinliersMask.rows; i++){
             if(right_RANSACinliersMask.at<bool>(i, 0) == 1){
 
                 r_pre_kpts_aft_H.push_back(r_pre_kpts_aft_F_aft_right_match[i]) ;
@@ -308,19 +327,109 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
             }
         }
 
-        ROS_INFO("Previous right keypoints after H: %d", r_pre_kpts_aft_H.size()) ;
-        ROS_INFO("Current right keypoints after H: %d", r_curr_kpts_aft_H.size()) ;
-        ROS_INFO("***********************************************") ;
+        // ROS_INFO("Previous right keypoints after H: %d", r_pre_kpts_aft_H.size()) ;
+        // ROS_INFO("Current right keypoints after H: %d", r_curr_kpts_aft_H.size()) ;
+        // ROS_INFO("-----------------------------------------------") ;
+
 
         /////////////////////////////////////////////////////////////////
         ///////////////////////Current side//////////////////////////////
         /////////////////////////////////////////////////////////////////
 
         // Compute the matchings of the current side
-        right_desc_matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED) ;
-        right_desc_matcher->knnMatch(r_pre_desc_aft_F, r_curr_desc, right_matches, k, cv::noArray(), true) ;
+        current_desc_matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED) ;
+        current_desc_matcher->knnMatch(r_curr_desc_aft_H, l_curr_desc_aft_H, current_matches, k, cv::noArray(), true) ;
+
+        // Obtaining keypoints, coordinates and descriptors of the current side after matching 
+        for(i = 0; i < current_matches.size(); i++){
+
+            r_curr_kpts_aft_H_aft_current_match.push_back(r_curr_kpts_aft_H[current_matches[i][0].queryIdx]) ;
+            r_curr_desc_aft_H_aft_current_match.push_back(r_curr_desc_aft_H.row(current_matches[i][0].queryIdx)) ;
+            r_curr_coord_aft_H_aft_current_match.push_back(r_curr_kpts_aft_H[current_matches[i][0].queryIdx].pt) ;
+
+            l_curr_kpts_aft_H_aft_current_match.push_back(l_curr_kpts_aft_H[current_matches[i][0].trainIdx]) ;
+            l_curr_desc_aft_H_aft_current_match.push_back(l_curr_desc_aft_H.row(current_matches[i][0].trainIdx)) ;
+            l_curr_coord_aft_H_aft_current_match.push_back(l_curr_kpts_aft_H[current_matches[i][0].trainIdx].pt) ;
+            
+        }
+
+        // ROS_INFO("Current right keypoints after H: %d", r_curr_kpts_aft_H.size()) ;
+        // ROS_INFO("Current left keypoints after H: %d", l_curr_kpts_aft_H.size()) ;
+        // ROS_INFO("Current right keypoints after H and after match: %d", r_curr_kpts_aft_H_aft_current_match.size() ) ;
+        // ROS_INFO("Current left keypoints after H and after match: %d", l_curr_kpts_aft_H_aft_current_match.size()) ;
 
 
+        // Compute current Fundamental Matrix
+        try{
+
+            current_F = cv::findFundamentalMat(r_curr_coord_aft_H_aft_current_match, l_curr_coord_aft_H_aft_current_match, current_RANSACinliersMask, CV_RANSAC, 3.f) ;
+
+        } catch (cv::Exception& e) {
+
+            ROS_ERROR("cv exception: %s", e.what()) ;
+
+        }
+
+        // Obtaining keypoints, coordinates and descriptors of the current side after fundamental matrix
+        for(i = 0; i < current_RANSACinliersMask.rows; i++){
+            if(current_RANSACinliersMask.at<bool>(i, 0) == 1){
+
+                r_curr_kpts_aft_F.push_back(r_curr_kpts_aft_H_aft_current_match[i]) ;
+                r_curr_desc_aft_F.push_back(r_curr_desc_aft_H_aft_current_match.row(i)) ;
+                r_curr_coord_aft_F.push_back(r_curr_coord_aft_H_aft_current_match[i]) ;
+
+                l_curr_kpts_aft_F.push_back(l_curr_kpts_aft_H_aft_current_match[i]) ;
+                l_curr_desc_aft_F.push_back(l_curr_desc_aft_H_aft_current_match.row(i)) ;
+                l_curr_coord_aft_F.push_back(l_curr_coord_aft_H_aft_current_match[i]) ;
+
+            }
+        }
+
+        ROS_INFO("Current right keypoints after F: %d", r_curr_kpts_aft_F.size()) ;
+        ROS_INFO("Current left keypoints after F: %d", l_curr_kpts_aft_F.size()) ;
+        ROS_INFO("Current left coord after F: %d", l_curr_coord_aft_F.size()) ;
+
+
+        int cont = 0 ;
+        std::vector<int> cont_i1 ; 
+        std::vector<int> cont_i2 ;
+        bool save_cont1 = false ;
+        bool save_cont2 = false ;
+
+        for(i = 0; i < l_curr_coord_aft_F.size(); i++){
+            for(j = 0; j < l_curr_coord_aft_H.size(); j++){
+                if((l_curr_coord_aft_F[i].x == l_curr_coord_aft_H[j].x) && (l_curr_coord_aft_F[i].y == l_curr_coord_aft_H[j].y)){
+                    if (std::find(std::begin(cont_i1), std::end(cont_i1), i) == std::end(cont_i1)){
+	                    save_cont1 = true ;
+                        cont_i1.push_back(i) ;
+                        // save l_pre_coord_after_H[j].x
+                        // save l_pre_coord_after_H[j].y
+                        break ;
+                    }                    
+                }
+            }
+            for(j = 0; j < r_curr_coord_aft_H.size(); j++){
+                if((r_curr_coord_aft_F[i].x == r_curr_coord_aft_H[j].x) && (r_curr_coord_aft_F[i].y == r_curr_coord_aft_H[j].y)){
+                    if (std::find(std::begin(cont_i2), std::end(cont_i2), i) == std::end(cont_i2)){
+	                    save_cont2 = true ;
+                        cont_i2.push_back(i) ;
+                        // save r_pre_coord_after_H[j].x
+                        // save r_pre_coord_after_H[j].y
+                        break ;
+                    }   
+                }
+            }
+            if(save_cont1 == true && save_cont2 == true){
+                cont++ ;
+                save_cont1 = false ;
+                save_cont2 = false ;
+            }
+        }
+
+        ROS_INFO("l_curr_coord_aft_H.size(): %d", l_curr_coord_aft_H.size()) ;
+        ROS_INFO("Contador: %d", cont) ;
+        ROS_INFO("Time between process: %f", Time_ImageCurrent - Time_ImagePrevious) ;
+        ROS_INFO("***********************************************") ;
 
     }
 
@@ -332,6 +441,8 @@ void imageCallback(const sensor_msgs::ImageConstPtr& l_image_msg, const sensor_m
     l_pre_desc = l_curr_desc ;
     r_pre_kpts = r_curr_kpts ;
     r_pre_desc = r_curr_desc ;
+
+    Time_ImagePrevious = Time_ImageCurrent ;
 }
 
 int main(int argc, char **argv)
